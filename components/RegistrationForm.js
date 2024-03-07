6c84ea7265364fd041ac413004d8c6cc82e6c334
Loris Polesana <zpolel@bbcag.ch>
2024-03-07 11:05:51 +0100

    continued coding RegistrationForm

--- components/RegistrationForm.js
+++ components/RegistrationForm.js
@@ -3,18 +3,25 @@ export default function RegistrationForm() {
         <div>
             <form>
                 <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    <label for="firstName" class="form-label">Vorname</label>
+                    <input type="text" class="form-control" id="firstName" aria-describedby="firstNameHelp" />
                 </div>
                 <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" />
+                    <label for="lastName" class="form-label">Nachname</label>
+                    <input type="text" class="form-control" id="lastName" />
                 </div>
-                <div class="mb-3 form-check">
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" />
+                </div>
+                <div class="mb-3">
+                    <label for="home-street-address" class="form-label">Strasse</label>
+                    <input type="home-street-address" class="form-control" id="home-street-address"/>
+                </div>
+                {/* <div class="mb-3 form-check">
                     <input type="checkbox" class="form-check-input" id="exampleCheck1" />
                         <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                </div>
+                </div> */}
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
         </div>
