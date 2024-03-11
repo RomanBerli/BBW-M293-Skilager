bed050b5388d97102f4855a462da68fa74877bf8
LoLo-work <loris.polesana@gmail.com>
2024-03-11 06:41:06 +0100

    Continued RegistrationForm

--- components/RegistrationForm.js
+++ components/RegistrationForm.js
@@ -1,7 +1,27 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from "react";
+
 export default function RegistrationForm() {
+    const [genderChoiche, setGenderCjoiche] = useState('')
+
+    const handleGenderChoiche = (e) => {
+        setGenderCjoiche(e.target.value)
+    }
+
     return (
-        <div>
+        <div className="container mt-5">
+            <h1>Angaben zum/zur Teilnehmer/in</h1>
+            <h1 className="border-bottom grey-border"></h1>
             <form>
+                <div class="mb-3">
+                    <label for="gender" class="form-label">Geschlecht</label>
+                    <select class="form-select" id="gender" aria-describedby="genderHelp">
+                        <option selected disabled hidden>Wählen Sie Ihr Geschlecht</option>
+                        <option value="m" onChange={handleGenderChoiche}>Männlich</option>
+                        <option value="w" onChange={handleGenderChoiche}>Weiblich</option>
+                        <option value="d" onChange={handleGenderChoiche}>Divers</option>
+                    </select>
+                </div>
                 <div class="mb-3">
                     <label for="firstName" class="form-label">Vorname</label>
                     <input type="text" class="form-control" id="firstName" aria-describedby="firstNameHelp" />
@@ -11,13 +31,117 @@ export default function RegistrationForm() {
                     <input type="text" class="form-control" id="lastName" />
                 </div>
                 <div class="mb-3">
-                    <label for="email" class="form-label">Email address</label>
+                    <label for="email" class="form-label">Email</label>
                     <input type="email" class="form-control" id="email" aria-describedby="emailHelp" />
                 </div>
                 <div class="mb-3">
                     <label for="home-street-address" class="form-label">Strasse</label>
-                    <input type="home-street-address" class="form-control" id="home-street-address"/>
+                    <input type="home-street-address" class="form-control" id="home-street-address" />
                 </div>
+                <div class="mb-3">
+                    <label for="home-zip-code" class="form-label">PLZ</label>
+                    <input type="home-zip-code" class="form-control" id="home-zip-code" />
+                </div>
+                <div class="mb-3">
+                    <label for="home-city" class="form-label">Ort</label>
+                    <input type="home-city" class="form-control" id="home-city" />
+                </div>
+                <div class="mb-3">
+                    <label for="AHV" class="form-label">AHV-Nummer</label>
+                    <input type="AHV" class="form-control" id="AHV" />
+                </div>
+                <div class="mb-3">
+                    <label for="birthday" class="form-label">Geburtsdatum</label>
+                    <input type="date" class="form-control" id="birthday" />
+                </div>
+                <div class="mb-3">
+                    <label for="phone" class="form-label">Telefonnummer</label>
+                    <input type="phone" class="form-control" id="phone" />
+                </div>
+
+
+
+                <h1>Angaben zu den Eltern</h1>
+                <h1 className="border-bottom grey-border"></h1>
+
+                <div class="mb-3">
+                    <label for="gender" class="form-label">Anrede</label>
+                    <select class="form-select" id="gender" aria-describedby="genderHelp">
+                        <option selected disabled hidden>Anrede</option>
+                        <option value="m" onChange={handleGenderChoiche}>Männlich</option>
+                        <option value="w" onChange={handleGenderChoiche}>Weiblich</option>
+                        <option value="d" onChange={handleGenderChoiche}>Divers</option>
+                    </select>
+                </div>
+                <div class="mb-3">
+                    <label for="firstName" class="form-label">Vorname</label>
+                    <input type="text" class="form-control" id="firstName" aria-describedby="firstNameHelp" />
+                </div>
+                <div class="mb-3">
+                    <label for="lastName" class="form-label">Nachname</label>
+                    <input type="text" class="form-control" id="lastName" />
+                </div>
+                <div class="mb-3">
+                    <label for="email-parents" class="form-label">Email</label>
+                    <input type="email" class="form-control" id="email-parents" />
+                </div>
+                <div class="mb-3">
+                    <label for="phone-parents" class="form-label">Telefonnummer</label>
+                    <input type="phone" class="form-control" id="phone-parents" />
+                </div>
+                <div class="mb-3">
+                    <label for="home-street-address" class="form-label">Strasse</label>
+                    <input type="home-street-address" class="form-control" id="home-street-address" />
+                </div>
+                <div class="mb-3">
+                    <label for="home-zip-code" class="form-label">PLZ</label>
+                    <input type="home-zip-code" class="form-control" id="home-zip-code" />
+                </div>
+                <div class="mb-3">
+                    <label for="home-city" class="form-label">Ort</label>
+                    <input type="home-city" class="form-control" id="home-city" />
+                </div>
+
+                <h4>Elternteil 2</h4>
+                <h1 className="border-bottom grey-border"></h1>
+                <div class="mb-3">
+                    <label for="gender" class="form-label">Anrede</label>
+                    <select class="form-select" id="gender" aria-describedby="genderHelp">
+                        <option selected disabled hidden>Anrede</option>
+                        <option value="m" onChange={handleGenderChoiche}>Männlich</option>
+                        <option value="w" onChange={handleGenderChoiche}>Weiblich</option>
+                        <option value="d" onChange={handleGenderChoiche}>Divers</option>
+                    </select>
+                </div>
+                <div class="mb-3">
+                    <label for="firstName" class="form-label">Vorname</label>
+                    <input type="text" class="form-control" id="firstName" aria-describedby="firstNameHelp" />
+                </div>
+                <div class="mb-3">
+                    <label for="lastName" class="form-label">Nachname</label>
+                    <input type="text" class="form-control" id="lastName" />
+                </div>
+                <div class="mb-3">
+                    <label for="email-parents" class="form-label">Email</label>
+                    <input type="email" class="form-control" id="email-parents" />
+                </div>
+                <div class="mb-3">
+                    <label for="phone-parents" class="form-label">Telefonnummer</label>
+                    <input type="phone" class="form-control" id="phone-parents" />
+                </div>
+                <div class="mb-3">
+                    <label for="home-street-address" class="form-label">Strasse</label>
+                    <input type="home-street-address" class="form-control" id="home-street-address" />
+                </div>
+                <div class="mb-3">
+                    <label for="home-zip-code" class="form-label">PLZ</label>
+                    <input type="home-zip-code" class="form-control" id="home-zip-code" />
+                </div>
+                <div class="mb-3">
+                    <label for="home-city" class="form-label">Ort</label>
+                    <input type="home-city" class="form-control" id="home-city" />
+                </div>
+
                 {/* <div class="mb-3 form-check">
                     <input type="checkbox" class="form-check-input" id="exampleCheck1" />
                         <label class="form-check-label" for="exampleCheck1">Check me out</label>
